3cd17ed1e6478f1699681b7c32bd2405093dad3e
emilianohg <emiliano4144@hotmail.com>
2021-07-30 03:40:56 -0600

    Proyecto limpio

---
+++ src/components/CardBoardGame.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card-board-game">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardBoardGame",
+  props: ['id', 'name', 'publisher', 'category', 'description', 'year', 'is_favorite'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CardBoardGame></CardBoardGame>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CardBoardGame from '../components/CardBoardGame';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CardBoardGame
   }
 }
 </script>
